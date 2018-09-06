c30026baebc93e6ea684c9db399e96660686854e
willson61 <31348545+willson61@users.noreply.github.com>
2018-09-06 07:46:21 -0600

    Empezando el Laboratorio

---
+++ app/src/main/java/com/example/sthephan/laboratorio_2_compresion/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sthephan.laboratorio_2_compresion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
